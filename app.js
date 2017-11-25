77c238e0c6a0c2c1e06f2259dde6a4b4a6d282ad
Stanislav Dashkov <stdashkov@mail.ru>
2017-11-25 20:26:23 +0300

    prepare to style

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var mysql      = require('mysql');
 var faker      = require("faker");
+var bodyParser = require("body-parser");
+var ejs        = require("ejs");
 var express    = require("express");
 var app        = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/css"));
 
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
@@ -9,16 +15,28 @@ var connection = mysql.createConnection({
   password : 'cnfcSQLSERVER92',
   database : 'email_db_app'
 });
-
 app.get("/", function(req, res) {
   // FIND COUNT USERS IN DATABASE
   var q = "SELECT COUNT(*) AS count FROM users";
   connection.query(q, function(err, results) {
     if(err) throw err;
     var count = results[0].count;
-    res.send("We have " + count + " users in our db");
+    res.render("home", {data: count});
   });
   // RESPOND WITH THAT ACC
+});
+
+app.post("/register", function(req, res) {
+
+  var email = req.body.email;
+  var person = {
+    email: req.body.email
+  };
+
+  connection.query('INSERT INTO users SET ?', person, function(err, result) {
+    if (err) throw err;
+    res.redirect("/");
+    });
 });
 
 app.get("/joke", function(req, res) {
@@ -30,6 +48,7 @@ app.get("/random", function(req, res) {
   var num = Math.floor(Math.random() * 10) + 1;
   res.send(num + ' ');
 });
+
 
 app.listen(8080, function() {
   console.log("app listening on port 8080!");
